8967f37a927c60f0baf501dd7b9530ccda16e096
bronzewhale <nguyendt456@gmail.com>
2023-04-03 23:56:49 +0700

    First commit with simple router

---
+++ src/components/navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
