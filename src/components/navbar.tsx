317f69c3eb2e3f713ab9d1ec866d93c73ad9f36d
khanhhhhhhh <vthanh0711@gmail.com>
2023-04-06 15:25:59 +0700

    Test

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,20 +3,25 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
+    
     <nav>
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/map">Map</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
+        <div className = "flex flex-col gap-[30px] p-[10px] text-black w-[70px] items-center h-[100vh]">
+          <div className = "flex flex-col gap-[40px] items-center justify-center">
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/map">Map</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </div>
+        </div>
       </ul>
     </nav>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Green: "#52B640",
+        lightGreen: "#A8FF8F"
+      }
+    },
+  },
+  plugins: [],
+}
+
